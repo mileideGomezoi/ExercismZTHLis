07541efce5ea04234f2695f944c1e6cebdf730f3
mileide_gomez <mileide.gomez@mylabserver.com>
2023-10-16 13:33:43 +0100

    solution completed two-fer exercise

---
+++ two-fer/two-fer.ts
@@ -0,0 +1,8 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer( friendName: string="you" ): string {  
+    return `One for ${friendName}, one for me.`
+
+}
